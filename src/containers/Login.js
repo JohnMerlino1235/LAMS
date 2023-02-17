127fad37b5647ccc4b6d90eac9c0f020e447a8f3
Hunter Wenglikowski <hunter.wenglikowski@hoverlay.com>
2023-02-17 13:40:21 -0500

    clean-up .css and html

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,98 +1,101 @@
 import { useFormik } from "formik";
 import axios from "axios";
-import './global.css';
-import Rectangle from 'react-rectangle';
-import sheep from './sheep.gif';
+import "./global.css";
+import Rectangle from "react-rectangle";
+import sheep from "./sheep.gif";
 
 function Login() {
-    const mystyle = {
-        color: "black",
-        padding: "-100px",
-        textAlign: "center",
-      };
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-        },
-        onSubmit: values => {
-            // console.log({ values });
-            axios.post(`http://127.0.0.1:5000//fetch_user_from_db/${values.email}`, {
-                'email': values.email
-              }).then(response => console.log(response.data));
-         },
-    });
-    return (
-        <form className={mystyle} onSubmit={formik.handleSubmit}>
-        <div className="Global-ellipses">
-            <Rectangle className="Global-ellipse-blue">
-                <Rectangle className="Global-ellipse-red">
-                </Rectangle>
-            </Rectangle>
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+
+    onSubmit: (values) => {
+      // console.log({ values });
+      axios
+        .post(`http://127.0.0.1:5000//fetch_user_from_db/${values.email}`, {
+          email: values.email,
+        })
+        .then((response) => console.log(response.data));
+    },
+
+  });
+  return (
+    <form className="root-syle" onSubmit={formik.handleSubmit}>
+      <div className="ellipses">
+        <Rectangle className="ellipse-blue">
+          <Rectangle className="ellipse-red"></Rectangle>
+        </Rectangle>
+      </div>
+
+      <div className="title">
+        <h1>LAMS</h1>
+        <p className="title-fix">Leg Activity Monitoring System</p>
+        <h2 className="title-fix">Sign Up</h2>
+      </div>
+
+      <div className="sheep">
+        <img src={sheep} className="sheep-image" alt="Loading..." />
+      </div>
+
+      <div className="inputs">
+        <input
+          className="input-box"
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          placeholder="Email"
+          required
+        />
+
+        <div className="forgot-password-text">
+          Don't have an account? Click the Sign-Up button below!
         </div>
-        <div style={mystyle}>
-            <h1>LAMS</h1>
-            <p>Leg Activity Monitoring System</p>
-            <h2>Log In</h2>
+
+        <input
+          className="input-box"
+          id="password"
+          name="password"
+          type="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          placeholder="Password"
+          required
+        />
+
+        <div className="forgot-password-text">
+          Forgot your password? Click the Forgot Password button to reset it!
         </div>
-        <div className="Global-sheep-div">
-            <img src={sheep} className="Global-sheep" alt="Loading..."/>
-        </div>
-            <div className="Global-test">
-                {/* <label htmlFor="email">Email Address</label> */}
-                <div>
-                    <input className="Global-input-box"
-                        id="email"
-                        name="email"
-                        type="email"
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                        placeholder="Email"
-                        required
-                    />
-                    <div className="Global-forgot-password-text">
-                        Don't have an account? Click the Sign-Up button below!
-                    </div>
-                </div>
-            </div>
-            <div className="Global-test-2">
-                {/* <label htmlFor="password">Password</label> */}
-                <div>
-                    <input className="Global-input-box"
-                        id="password"
-                        name="password"
-                        type={"password"}
-                        onChange={formik.handleChange}
-                        value={formik.values.password}
-                        placeholder="Password"
-                        required
-                    />
-                    <div className="Global-forgot-password-text">
-                        Forgot your password? Click the Forgot Password button to reset it!
-                    </div>
-                </div>
-            </div>  
-            <div className="Global-button-login-div">
-                <button class= "Global-button-login" type="submit" onChange={formik.onSubmit}>
-                    <a class="Global-button-href-styling" href="/log-in">
-                        Login
-                    </a>
-                </button>
-            </div>
-            <div className="Global-button-div">
-                <button class="Global-button-sign-up" type="button">
-                    <a class="Global-button-href-styling" href="/sign-up">
-                        Sign-Up
-                    </a>
-                </button>            
-                <button class="Global-button-forgot-password" type="button">
-                    <a class="Global-button-href-styling" href="/forgot-password">
-                        Forgot Password?
-                    </a>
-                </button>
-            </div>
-        </form>
-    );
-};
+      </div>
+
+      <div className="buttons">
+        <button className="button-style" type="submit">
+          <a class="button-text" href="/log-in">
+            Login
+          </a>
+        </button>
+      </div>
+
+      <div className="buttons">
+        <button className="button-style" type="submit">
+          <a class="button-text" href="/log-in">
+            Sign-Up
+          </a>
+        </button>
+
+        <button className="button-style" type="submit">
+          <a class="button-text" href="/log-in">
+            Forgot Password?
+          </a>
+        </button>
+      </div>
+
+    </form>
+  );
+}
 
 export default Login;
--- src/containers/SignUpPage.js
+++ src/containers/SignUpPage.js
@@ -7,12 +7,6 @@ import sheep from './sheep.gif';
 
 function SignUpPage() {
 
-    //once we decide to format it to "Login.js":
-    const mystyle = {
-        color: "black",
-        padding: "-100px",
-        textAlign: "center",
-      };
     const formik = useFormik({
         initialValues: {
             email: '',
@@ -30,81 +24,74 @@ function SignUpPage() {
     });
 
   return (
-    <form className={mystyle} onSubmit={formik.handleSubmit}>
-      <div className="Global-ellipses">
-          <Rectangle className="Global-ellipse-blue">
-            <Rectangle className="Global-ellipse-red">
-            </Rectangle>
-          </Rectangle>
+    <form className="root-syle" onSubmit={formik.handleSubmit}>
+      <div className="ellipses">
+        <Rectangle className="ellipse-blue">
+          <Rectangle className="ellipse-red"></Rectangle>
+        </Rectangle>
       </div>
-      <div style={mystyle}>
-          <h1>LAMS</h1>
-          <p>Leg Activity Monitoring System</p>
-          <h2>Sign Up</h2>
+
+      <div className="title">
+        <h1>LAMS</h1>
+        <p className="title-fix">Leg Activity Monitoring System</p>
+        <h2 className="title-fix">Sign Up</h2>
       </div>
-      <div className="Global-sheep-div">
-            <img src={sheep} className="Global-sheep" alt="Loading..."/>
+
+      <div className="sheep">
+        <img src={sheep} className="sheep-image" alt="Loading..." />
       </div>
-      <div className="Global-test">
-        <div>
-          <input className="Global-input-box"
-            id='firstName'
-            name='firstName'
-            type='text'
-            onChange={formik.handleChange}
-            value={formik.values.firstName}
-            placeholder='First Name'
-            required
-            />
-        </div>
+
+      <div className="inputs">
+        <input
+          className="input-box"
+          id="firstName"
+          name="firstName"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.firstName}
+          placeholder="First Name"
+          required
+        />
+        <input
+          className="input-box"
+          id="lastName"
+          name="lastName"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.lastName}
+          placeholder="Last Name"
+          required
+        />
+        <input
+          className="input-box"
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          placeholder="Email"
+          required
+        />
+        <input
+          className="input-box"
+          id="password"
+          name="password"
+          type="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          placeholder="Password"
+          required
+        />
       </div>
-      <div className="Global-test-2">
-        <div>
-          <input className="Global-input-box"
-            id='lastName'
-            name='lastName'
-            type='text'
-            onChange={formik.handleChange}
-            value={formik.values.lastName}
-            placeholder='Last Name'
-            required
-            />
-        </div>
-      </div>
-      <div className="Global-test-2">
-        <div>
-          <input className="Global-input-box"
-            id='email'
-            name='email'
-            type='email'
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            placeholder='Email'
-            required
-            />
-        </div>
-      </div>
-      <div className="Global-test-2">
-        <div>
-          <input className="Global-input-box"
-            id='password'
-            name='password'
-            type='password'
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            placeholder='Password'
-            required
-            />
-        </div>
-      </div>
-      <div className="Global-button-div">
-        <button class="Global-button-sign-up" type="submit">
-          <a class="Global-button-href-styling" href="/log-in">
+      
+      <div className="buttons">
+        <button className="button-style" type="submit">
+          <a class="button-text" href="/log-in">
             Create Account
           </a>
         </button>
-        <button class="Global-button-sign-up" type="button">
-          <a class="Global-button-href-styling" href="/log-in">
+        <button className="button-style" type="submit">
+          <a class="button-text" href="/log-in">
             Cancel
           </a>
         </button>
