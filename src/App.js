1ed8cd163ed3ab025cb6da40e5dd420a447340a7
Hunter Wenglikowski <hunter.wenglikowski@hoverlay.com>
2023-04-02 11:45:09 -0400

    refactor

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
-import Login from './containers/Login';
-import Welcome from './containers/Welcome';
+import Login from './containers/Login/Login';
+import Welcome from './containers/Welcome/Welcome';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignUpPage from './containers/SignUpPage';
-import ForgotPasswordPage from './containers/ForgotPassword';
-import PersonalDetailsPage from './containers/PersonalDetailsPage';
-import HomePage from './containers/HomePage';
-import Profile from './containers/Profile';
-import Messages from './containers/Messages';
-import SettingsHelp from './containers/SettingsHelp';
-import About from './containers/About';
-import Calibrate from './containers/CalibratePage';
-import Instructions from './containers/Instructions';
-import Exercise from './containers/Exercise';
-import MyNotebook from './containers/MyNotebook';
-import Results from './containers/Results';
+import SignUpPage from './containers/SignUp/SignUpPage';
+import ForgotPasswordPage from './containers/ForgotPassword/ForgotPassword';
+import PersonalDetailsPage from './containers/PersonalDetails/PersonalDetailsPage';
+import HomePage from './containers/HomePage/HomePage';
+import Profile from './containers/Profile/Profile';
+import Messages from './containers/Messages/Messages';
+import SettingsHelp from './containers/SettingsHelp/SettingsHelp';
+import About from './containers/About/About';
+import Calibrate from './containers/Calibrate/CalibratePage';
+import Instructions from './containers/Instructions/Instructions';
+import Exercise from './containers/Exercise/Exercise';
+import MyNotebook from './containers/MyNotebook/MyNotebook';
+import Results from './containers/Results/Results';
+import "./containers/css/global.css";
+import "./containers/css/home-page.css";
+import "./containers/css/hamburger-menu.css";
 
 function App() {
   return (
--- src/containers/Welcome.js
+++
@@ -1,5 +0,0 @@
-function Welcome(props) {
-  return <hi1>Hello, {props.name}</hi1>;
-}
-
-export default Welcome;
