e65d97f46806651853d6baf8005b17251a9236a4
JohnMerlino1235 <john.merlino1235@gmail.com>
2023-04-06 16:05:00 -0400

    SignUp form fix

--- src/containers/SignUp/SignUpPage.js
+++ src/containers/SignUp/SignUpPage.js
@@ -6,49 +6,18 @@ import Ellipses from "../shared/Ellipses";
 import Header from '../shared/Header';
 import InputsSignUp from './InputsSignUp';
 import ButtonsSignUp from './ButtonsSignUp';
+import SignUpForm from './SignupForm';
 
 
 function SignUpPage() {
-  const navigate = useNavigate();
-
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-      firstName: "",
-      lastName: "",
-    },
-
-    onSubmit: (values) => {
-      axios
-        .post("http://127.0.0.1:5000//signup", {
-          email: values.email,
-          password: values.password,
-          first_name: values.firstName,
-          last_name: values.lastName,
-        })
-        .then((response) => {
-          if (response.data.success) {
-            navigate("/log-in");
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-    },
-  })
-
   return (
-    <form className="root-syle" onSubmit={formik.handleSubmit}>
+    <div className="root-syle">
       <Ellipses/>
 
       <Header/>
 
-      <InputsSignUp/>
-
-      <ButtonsSignUp/>
-
-    </form>
+      <SignUpForm />
+    </div>
   );
 }
 
---
+++ src/containers/SignUp/SignupForm.js
@@ -0,0 +1,100 @@
+import axios from "axios";
+import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
+
+function SignUpForm() {
+
+    const navigate = useNavigate();
+
+    const formik = useFormik({
+        initialValues: {
+          email: "",
+          password: "",
+          firstName: "",
+          lastName: "",
+        },
+    
+        onSubmit: (values) => {
+          axios
+            .post("http://127.0.0.1:5000//signup", {
+              email: values.email,
+              password: values.password,
+              first_name: values.firstName,
+              last_name: values.lastName,
+            })
+            .then((response) => {
+              if (response.data.success) {
+                navigate("/log-in");
+              }
+            })
+            .catch((error) => {
+                console.log(values);
+              console.log(error);
+            })
+        },
+      })
+
+    return(
+        <form onSubmit={formik.handleSubmit}>
+    <div className="inputs">
+    <input
+      className="input-box"
+      id="firstName"
+      name="firstName"
+      type="text"
+      onChange={formik.handleChange}
+      value={formik.values.firstName}
+      placeholder="First Name"
+      required
+    />
+    <input
+      className="input-box"
+      id="lastName"
+      name="lastName"
+      type="text"
+      onChange={formik.handleChange}
+      value={formik.values.lastName}
+      placeholder="Last Name"
+      required
+    />
+    <input
+      className="input-box"
+      id="email"
+      name="email"
+      type="email"
+      onChange={formik.handleChange}
+      value={formik.values.email}
+      placeholder="Email"
+      required
+    />
+    <input
+      className="input-box"
+      id="password"
+      name="password"
+      type="password"
+      onChange={formik.handleChange}
+      value={formik.values.password}
+      placeholder="Password"
+      required
+    />
+  </div>
+  <div className="buttons">
+      <button className="button-style" type="submit">
+        <a class="button-text">
+        Create Account
+        </a>
+      </button>
+      <button
+        className="button-style"
+        onClick={() => navigate('/log-in')}
+      >
+        <a class="button-text">
+        Cancel
+        </a>
+      </button>
+    </div>
+            </form>
+    )
+}
+
+export default SignUpForm;
