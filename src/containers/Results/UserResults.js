d2c1a5fcb36068d255b5c63cf5c93aa3b34cc5c2
JohnMerlino1235 <john.merlino1235@gmail.com>
2023-04-07 19:24:49 -0400

    FIXED FINAL COMMIT

--- src/containers/Results/UserResults.js
+++ src/containers/Results/UserResults.js
@@ -6,38 +6,34 @@ import { toast } from 'react-toastify';
 function UserResults() {
   const params = useParams()
   const [imageUrl, setImageUrl] = useState('');
+  const [showImageButton, setShowImageButton] = useState(false);
 
-  const getImage = () => {
+  const getImagePicture = () => {
+    axios.get(`http://127.0.0.1:5000//get_image/${params.email}`, { responseType: 'blob' })
+      .then(response => {
+        const imageUrl = URL.createObjectURL(response.data);
+        setImageUrl(imageUrl);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  const createImage = () => {
     axios.post('http://127.0.0.1:5000//get_data', {email: params.email})
     .then(response => {
       if (response.data.success) {
-        axios.post(`http://127.0.0.1:5000//get_image/${params.email}`, { responseType: 'blob', })
-        .then(response => {
-            const imageUrl = URL.createObjectURL(response.data);
-            setImageUrl(imageUrl);
-            console.log(imageUrl);
-          toast.success('Results generated!', {
-              position: toast.POSITION.TOP_CENTER,
-              autoClose: 3000,
-              hideProgressBar: true,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-            });  
-        })
-        .catch(error => {
-          console.log(error);
-          toast.error(`${error}`, {
-              position: toast.POSITION.TOP_CENTER,
-              autoClose: 3000, // 3 seconds
-              hideProgressBar: true,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-            });        
-        });  
+        setShowImageButton(true);
+        toast.success('Graph created!', {
+            position: toast.POSITION.TOP_CENTER,
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+
       }
     }).catch(error => {
       toast.error(`${error}`, {
@@ -54,14 +50,19 @@ function UserResults() {
 
   useEffect(() => {
     if (params.email) {
-        getImage()
+        createImage()
     }
   }, [params.email])
 
   return(
-        <div className="buttons-home">
-            {imageUrl ? <img src={imageUrl} alt={imageUrl} /> : <h2 title='Loading'/> }
-      </div>
+    //     <div className="buttons-home">
+    //         {imageUrl ? <img src={imageUrl} alt={imageUrl} /> : <h2 title='Loading'/> }
+    //   </div>
+        <div>
+        {showImageButton && <button onClick={getImagePicture}>Get Image</button>}
+        {imageUrl && <img src={imageUrl} alt="image" />}
+        </div>
+
     )
 }
 
