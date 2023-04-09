d7eba4da2fe68eba18c3bc6b83ab236b6661bc75
JohnMerlino1235 <john.merlino1235@gmail.com>
2023-04-09 16:13:38 -0400

    Scaffolding for determining exercise fetching

--- src/containers/HomePage/HomePageComponent.js
+++ src/containers/HomePage/HomePageComponent.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import moment from 'moment/moment';
 
 const HomeScreen = () => {
   const days = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
-  const completedDays = ['M', 'W', 'F']; // This could be an array of the days that have been completed
+//   const completedDays = ['M', 'W', 'F']; // This could be an array of the days that have been completed
+  const [completedDays, setCompletedDays] = useState([]);
 
+  useEffect(() => {
+    const today = new Date();
+    const startOfWeek = new Date(today.setDate(today.getDate() - today.getDay() + 1)); // get the start of the week (Monday)
+    const endOfWeek = new Date(today.setDate(today.getDate() - today.getDay() + 7)); // get the end of the week (Sunday)
+    console.log('today', today);
+    console.log('start', startOfWeek);
+    console.log('end', endOfWeek);
+    // filter the dates to only those that fall within the current week
+    // const currentWeekDates = data.filter(date => {
+    //     const dateObj = new Date(date);
+    //     return dateObj >= startOfWeek && dateObj <= endOfWeek;
+    // }); 
+    // setCompletedDays(currentWeekDays);   
+  }, []);
   return (
     <div className="home-screen">
       <h1 className='instructions-page-header'>Welcome Back!</h1>
